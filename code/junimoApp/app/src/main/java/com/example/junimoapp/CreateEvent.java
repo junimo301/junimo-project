0bf2214644f0997763f3b49074dc667b044b3c64
Farzana Nasim <fnasim1@ualberta.ca>
2026-03-07 00:46:37 -0700

    CreateEvent + User stories

--- code/junimoApp/app/src/main/java/com/example/junimoapp/CreateEvent.java
+++ code/junimoApp/app/src/main/java/com/example/junimoapp/CreateEvent.java
@@ -1,10 +1,34 @@
 package com.example.junimoapp;
 
+import android.location.Location;
+import android.media.metrics.Event;
+
+import java.util.Date;
+
 public class CreateEvent extends OrganizerStart {
     /*
     * User stories:
     * US 02.01.01 As an organizer I want to create a new event and generate a unique promotional QR code that links to the event description and event poster in the app.
     * US 02.01.04 As an organizer, I want to set a registration period.
     * US 02.03.01 As an organizer I want to OPTIONALLY limit the number of entrants who can join my waiting list.
+    *
+    * Create Event: title, description, date, location, max capcity, registration period, waiting list, price, geo location, poster
+    *
     * */
+
+    private String title;
+    private String description;
+    private Date startDate;  //registration period
+    private Date endDate;    //registration period
+    private Location location;
+    private int maxCapacity;
+    private int waitingListLimit;
+    private int price;
+    private Location geoLocation;
+    private String poster;
+
+    Event myEvent = new Event(title, description, startDate, endDate, location, maxCapacity, waitingListLimit, price, geoLocation, poster);
+
+
+
 }
--- code/junimoApp/app/src/main/java/com/example/junimoapp/OrganizerStart.java
+++ code/junimoApp/app/src/main/java/com/example/junimoapp/OrganizerStart.java
@@ -2,8 +2,7 @@ package com.example.junimoapp;
 
 public class OrganizerStart {
     /*
-    *
-    *
+    * Connect all screens and tasks to this page
     *
     * */
 }
