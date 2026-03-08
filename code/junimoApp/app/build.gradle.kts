c144001764a4d150130e9146cf2d2467d33675fb
kmarshh <kpmarsh@ualberta.ca>
2026-03-08 15:53:13 -0600

    user homepage, invitations, event details, profile stuffs. missing a couple of files for the events still

--- code/junimoApp/app/build.gradle.kts
+++ code/junimoApp/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.firebase:firebase-firestore:26.1.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
