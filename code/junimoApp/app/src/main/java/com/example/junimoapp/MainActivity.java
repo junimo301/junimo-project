743b7d419d9195b1310f5b7dcfe1fe090b19eb21
ahorsema-byte <ahorsema@ualberta.ca>
2026-03-08 21:34:57 -0600

    firestore things

--- code/junimoApp/app/src/main/java/com/example/junimoapp/MainActivity.java
+++ code/junimoApp/app/src/main/java/com/example/junimoapp/MainActivity.java
@@ -8,7 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+
+    private CollectionReference eventsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        db = FirebaseFirestore.getInstance();
+        eventsRef = db.collection("events");
     }
 }
