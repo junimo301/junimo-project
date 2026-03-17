69373b9bb453dbd8e76bcf22dbf920c8854c9738
ahorsema-byte <ahorsema@ualberta.ca>
2026-03-17 15:09:54 -0600

    fixed organizer start screen
    someone should look at that layout though, we got the repeating header

--- code/junimoApp/app/src/main/java/com/example/junimoapp/OrganizerStartScreen.java
+++ code/junimoApp/app/src/main/java/com/example/junimoapp/OrganizerStartScreen.java
@@ -3,8 +3,11 @@ package com.example.junimoapp;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,8 +20,15 @@ import com.example.junimoapp.Organizer.CreateEvent;
 import com.example.junimoapp.Organizer.EventData;
 import com.example.junimoapp.Organizer.ListOfMyEvents;
 import com.example.junimoapp.Organizer.SelectAnEvent;
+import com.example.junimoapp.firebase.FirebaseManager;
 import com.example.junimoapp.models.Event;
+import com.example.junimoapp.models.User;
+import com.example.junimoapp.models.UserSession;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -36,6 +46,8 @@ public class OrganizerStartScreen extends AppCompatActivity {
     private RecyclerView scrollable;
     private ListOfMyEvents myEvents;
     private List<Event> eventList;
+    private FirebaseFirestore db;
+
 
 
     /**
@@ -74,9 +86,14 @@ public class OrganizerStartScreen extends AppCompatActivity {
         //----------VIEW MY EVENTS-------------------
         scrollable = findViewById(R.id.scrollable);
         scrollable.setLayoutManager(new LinearLayoutManager(this));
-        eventList = EventData.listOfEvents();
+
+        eventList= new ArrayList<>();
+
         myEvents = new ListOfMyEvents(eventList);
         scrollable.setAdapter(myEvents);
+
+        db = FirebaseManager.getDB();
+        loadEvents();
     }
 
     /**
@@ -90,5 +107,52 @@ public class OrganizerStartScreen extends AppCompatActivity {
         eventList= EventData.listOfEvents();
         myEvents = new ListOfMyEvents((eventList));
         scrollable.setAdapter(myEvents);
+    }
+    private void loadEvents() {
+        Log.d("organizer browse activity","on load events of browse activity");
+        db.collection("events").get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    // clear any old data before loading fresh results
+                    eventList.clear();
+                    User currentUser = UserSession.getCurrentUser(); //get current user
+
+                    for (DocumentSnapshot doc : queryDocumentSnapshots) {
+                        //Fields in events
+                        String title = doc.getString("title");
+
+                        String description = doc.getString("description");
+                        String startDate = doc.getString("startDate");
+                        String endDate = doc.getString("endDate");
+                        String dateEvent = doc.getString("dateEvent");
+                        int maxCapacity = (doc.getLong("maxCapacity")).intValue();
+                        int waitingListLimit = (doc.getLong("waitingListLimit")).intValue();
+                        double price = doc.getDouble("price");
+                        GeoPoint geoLocation = doc.getGeoPoint("geoLocation"); //geoPoint is a type apparently? seems helpful??
+                        String poster = doc.getString("poster");
+                        String eventID = doc.getString("eventID");
+                        String eventLocation = doc.getString("eventLocation");
+                        String organizerID = doc.getString("organizerID");
+
+                        Event event = new Event(title, description, startDate, endDate, dateEvent, maxCapacity, waitingListLimit, price, geoLocation, poster, eventID, eventLocation, organizerID);
+                        if(organizerID != null){
+                            if(event.getOrganizerID().equals(currentUser.getDeviceId())) {
+                                eventList.add(event);
+                            }
+                        }
+                        else {
+                            Log.d("organizer browse activity","organizer ID was null in event");
+                        }
+
+                        Log.d("organizer browse activity",eventList.toString());
+                    }
+                    myEvents.notifyDataSetChanged();
+                })
+                .addOnFailureListener(e -> {
+                    // log the error for debugging in Logcat
+                    Log.e("Firebase", "Failed to load events", e);
+                    // show a brief message to the admin so they know something went wrong
+                    Toast.makeText(this, "Failed to load events", Toast.LENGTH_SHORT).show();
+                });
+
     }
 }
